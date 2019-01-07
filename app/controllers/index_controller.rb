f76bcac79d2492f2859df09f6ba2e2fd95fe13a7
ivanliu <liulang_129@126.com>
2019-01-07 15:22:40 +0800

    fix resend mail,modify google auth instead of phone in step 2 verify，added page error messages

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,6 +5,7 @@ class IndexController < ApplicationController
     return redirect_to new_account_session_url unless account_signed_in?
 
     redirect_to security_path if current_account.level == 1
+    redirect_to security_path unless current_account.otp_enabled
     # redirect_to new_profile_path if current_account.level == 2 && current_account.documents.blank?
   end
 end
